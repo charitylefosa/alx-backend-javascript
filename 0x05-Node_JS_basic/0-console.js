1c8adb9ac73cb6d9c024b30665caca2c009f4de3
charitylefosa <charitylefosa@gmail.com>
2024-04-30 18:45:50 -0700

    finally done

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Phefumlela Madyo <https://github.com/PhefuM>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
